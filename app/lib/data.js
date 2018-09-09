58ba90c4a51c4dbf526c1b7cfc9164a06bc960d5
Kevin M. Rhodes <kevin@zark.io>
2018-09-09 11:36:08 -0500

    Add CRUD with disk repo

---
+++ app/lib/data.js
@@ -0,0 +1,92 @@
+/*
+ * Library for CRUD data operations to file
+ */
+
+var fs = require('fs');
+var path = require('path');
+
+var lib = {};
+
+lib.baseDir = path.join(__dirname,'/../.data/');
+
+function getPath(dir, file) {
+    return lib.baseDir+dir+'/'+file+'.json';
+};
+
+// create
+lib.create = function(dir,file,data,callback){
+
+    fs.open(getPath(dir,file), 'wx', (err, fileDescriptor) => {
+    if(!err && fileDescriptor){
+      const stringData = JSON.stringify(data);
+      fs.writeFile(fileDescriptor, stringData, (err) => {
+        if(!err){
+          fs.close(fileDescriptor, (err) => {
+            if(!err){
+              callback(false);
+            } else {
+              callback('Error closing new file');
+            }
+          });
+        } else {
+          callback('Error writing to new file');
+        }
+      });
+    } else {
+      callback('Could not create new file, it may already exist');
+    }
+  });
+};
+
+// read
+lib.read = function(dir,file,callback){
+
+    fs.readFile(getPath(dir,file), 'utf8', (err,data) => {
+    callback(err,data);
+  });
+};
+
+// update
+lib.update = function(dir,file,data,callback){
+
+  fs.open(getPath(dir,file), 'r+', (err, fileDescriptor) => {
+    if(!err && fileDescriptor){
+      const stringData = JSON.stringify(data);
+      fs.truncate(fileDescriptor, (err) => {
+        if(!err){
+          fs.writeFile(fileDescriptor, stringData, (err) => {
+            if(!err){
+              fs.close(fileDescriptor, (err) => {
+                if(!err){
+                  callback(false);
+                } else {
+                  callback('Error closing existing file');
+                }
+              });
+            } else {
+              callback('Error writing to existing file');
+            }
+          });
+        } else {
+          callback('Error truncating file');
+        }
+      });
+    } else {
+      callback('Could not open file for updating, it may not exist');
+    }
+  });
+};
+
+// delete
+lib.delete = function(dir,file,callback){
+
+  fs.unlink(getPath(dir,file), 'r+', (err, fileDescriptor) => {
+    if(!err){
+        callback(false);
+    } else {
+        callback('Error deleting file');
+    }
+  });
+};
+
+module.exports = lib;
